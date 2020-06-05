577599abda79455cd9a2cb81c2b6ac042019e07b
MarvinKweyu <mkweyu1@gmail.com>
2020-06-05 16:03:15 +0300

    Create dialog on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialogConfig, MatDialog} from "@angular/material/dialog";
+// import {DialogBodyComponent} from "./dialog-body/dialog-body.component";
+import {DialogOverviewExample} from "./dialog-body/dialog-overview-example";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'working-dialogs';
+  name: any;
+  constructor(private matDialog: MatDialog) {
+  }
+
+  openDialog(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = "some data"; // optionally an object
+    let dialogRef = this.matDialog.open(DialogOverviewExample, dialogConfig);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
+import {DialogOverviewExample} from "./dialog-body/dialog-overview-example";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+// import { DialogBodyComponent } from './dialog-body/dialog-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogOverviewExample,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
